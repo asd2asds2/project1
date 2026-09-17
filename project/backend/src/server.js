cc064139d79db2c5bbe8bff634922a29fdc11e6f
asd2asds2 <ainsworthhca@gmail.com>
2026-09-17 13:49:30 +0500

    Add files via upload

--- project/backend/src/server.js
+++ project/backend/src/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const healthRoutes = require('./routes/health');
 const authRoutes = require('./routes/auth');
-const branchesRoutes = require('./routes/branches');
-const purchasesRoutes = require('./routes/purchases');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,8 +11,6 @@ app.use(express.json());
 
 app.use('/api', healthRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/branches', branchesRoutes);
-app.use('/api/purchases', purchasesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Backend запущен на порту ${PORT}`);
