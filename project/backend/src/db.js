51b1560ba4291ee4e6765a977cc9da151a646be3
asd2asds2 <ainsworthhca@gmail.com>
2026-09-17 13:53:54 +0500

    Delete project directory

--- project/backend/src/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-
-// DATABASE_URL приходит из docker-compose.yml (переменная окружения контейнера)
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-module.exports = pool;
